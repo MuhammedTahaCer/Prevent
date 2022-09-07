00b9bbb6e46e4ef9fe3d251c595db335442c5a7c
Muhammed Taha <cermuhammed720@gmail.com>
2022-09-07 16:03:21 +0300

    LastUpdate

--- ScreenSaveBlock/ScreenSaveBlock/MainPage.xaml.cs
+++ ScreenSaveBlock/ScreenSaveBlock/MainPage.xaml.cs
@@ -13,35 +13,11 @@ namespace PreventScreenSave
     public partial class MainPage : ContentPage
     {
 
-        public static bool IsCaptureSupported
-            => PlatformIsCaptureSupported;
-
-        public static Task<ScreenshotResult> CaptureAsync()
-        {
-            if (!IsCaptureSupported)
-                throw new FeatureNotSupportedException();
-
-            return PlatformCaptureAsync();
-        }
-
-        public partial class ScreenshotResult
-        {
-            public int Width { get; }
-
-            public int Height { get; }
-
-            public Task<Stream> OpenReadAsync(ScreenshotFormat format = ScreenshotFormat.Png) =>
-                PlatformOpenReadAsync(format);
-        }
-
-        public enum ScreenshotFormat
-        {
-            Png,
-            Jpeg
-        }
+        
         public MainPage()
         {
             InitializeComponent();
+
 
         }
 
@@ -50,27 +26,27 @@ namespace PreventScreenSave
              Navigation.PushAsync(new Page2());
         }
 
-      //async  private Task ListenEvent(object sender, EventArgs e)
-      //  {
+        async private Task ListenEvent(object sender, EventArgs e)
+        {
 
-      //          if (Screenshot.IsCaptureSupported)
-      //          {
-      //              try
-      //              {
-      //                  var screenshot = await Screenshot.CaptureAsync();
-      //                  var stream = await screenshot.OpenReadAsync();
+            if (Screenshot.IsCaptureSupported)
+            {
+                try
+                {
+                    var screenshot = await Screenshot.CaptureAsync();
+                    var stream = await screenshot.OpenReadAsync();
 
-      //                  var captured = ImageSource.FromStream(() => stream);
-      //                  captured = null;
-      //              }
-      //              catch (InvalidOperationException)
-      //              {
-      //                await  DisplayAlert("Uyarı!", "Yapmak istediğiniz işlem uyg tarafından desteklenmemektedir!", "Geri Dön");
-      //              }
-      //          }
-      //  }
-
-
+                    var captured = ImageSource.FromStream(() => stream);
+                    captured = null;
+                }
+                catch (InvalidOperationException)
+                {
+                    await DisplayAlert("Uyarı!", "Yapmak istediğiniz işlem uyg tarafından desteklenmemektedir!", "Geri Dön");
+                }
             }
+        }
+
+
+    }
 
         }
